92c9e02609d122bb2e9d8394accbb0325862f00b
Isaque de Oliveira Braz <157842012+IsaqueBraz17@users.noreply.github.com>
2025-01-08 10:50:27 -0300

    Add files via upload

---
+++ Gerador_de_Tabuada.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gerador_de_Tabuada
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Gerador de tabuada
+
+            Console.Write("Digite um Número: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Até qual número deseja multiplicar ? ");
+            int limite = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+            Console.WriteLine("Tabuada do " + numero + "!");
+
+            for (int i = 0; i <= limite; i++)
+            {
+                int resultado = numero * i;
+                Console.WriteLine(numero + " x " + i + " = " + resultado);
+            }
+        }
+    }
+}
